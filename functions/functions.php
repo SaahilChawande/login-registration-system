9d49c436ca9a4e1a09a127e582e5195cdec977e0
SaahilChawande <chawandesaahil600@gmail.com>
2019-03-09 00:19:32 +0530

    General Helper Functions

--- functions/functions.php
+++ functions/functions.php
@@ -1,2 +1,32 @@
 <?php
 
+// Helper Functions
+function clean($string) {
+    return htmlentities($string);
+}
+
+function redirect($location)    {
+    header("Location: {$location}");
+}
+
+function set_message($message)  {
+    if (!empty($message))   {
+        $_SESSION['message'] = $message;
+    }   else    {
+        $message = "";
+    }
+}
+
+function display_message()  {
+    if (issset($_SESSION['message']))   {
+        echo $_SESSION['message'];
+        unset($_SESSION['message']);
+    }
+}
+
+function token_generator()  {
+    $token = $_SESSION['token'] = md5(uniqid(mt_rand(), true));
+    return $token;
+}
+
+// Validation Functions
