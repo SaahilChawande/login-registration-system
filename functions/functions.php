09c645e65c2611a85b613563efe0bd831bc18c4e
SaahilChawande <chawandesaahil600@gmail.com>
2019-03-09 12:20:23 +0530

    Validating input fields part 1

--- functions/functions.php
+++ functions/functions.php
@@ -32,6 +32,11 @@ function token_generator()  {
 // Validation Functions
 
 function validate_user_registration()   {
+    $errors = [];
+
+    $min = 3;
+    $max = 20;
+
     if ($_SERVER['REQUEST_METHOD'] == "POST")   {
         $first_name = clean($_POST['first_name']);
         $last_name = clean($_POST['last_name']);
@@ -39,5 +44,35 @@ function validate_user_registration()   {
         $email = clean($_POST['email']);
         $password = clean($_POST['password']);
         $confirm_password = clean($_POST['confirm_password']);
+
+        if (strlen($first_name) < $min || strlen($first_name) > $max)    {
+            $errors[] = "Your first name should be between {$min} and {$max} characters";
+        }
+
+        if (empty($first_name)) {
+            $errors[] = "Your first name cannot be empty";
+        }
+
+        if (strlen($last_name) < $min || strlen($last_name) > $max)    {
+            $errors[] = "Your last name should be between {$min} and {$max} characters";
+        }
+
+        if (empty($last_name))  {
+            $errors[] = "Your last name cannot be empty";
+        }
+
+        if (strlen($username) < $min || strlen($username) > $max)    {
+            $errors[] = "Your username should be between {$min} and {$max} characters";
+        }
+
+        if (empty($username))   {
+            $errors[] = "Your username cannot be empty";
+        }
+
+        if (!empty($errors))    {
+            foreach ($errors as $error) {
+                echo $error;
+            }
+        }
     }
 }
